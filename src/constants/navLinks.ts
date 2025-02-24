750d80a9d3f3b8add97dd8a65eede58e7f2341ab
The-BoxHead-Guy <astralgmd@gmail.com>
2025-02-23 21:12:18 -0400

    Adds navigation links
    Defines an array of navigation links with name and href properties.
    
    This allows for easy management and modification of the navigation structure.

---
+++ src/constants/navLinks.ts
@@ -0,0 +1,14 @@
+type NavLink = {
+  name: string;
+  href: string;
+};
+
+const navLinks: Array<NavLink> = [
+  { name: "Collections", href: "#" },
+  { name: "Men", href: "#" },
+  { name: "Women", href: "#" },
+  { name: "About", href: "#" },
+  { name: "Contact", href: "#" },
+];
+
+export default navLinks;
