fab5ac988bc1897485e84785048a9a4434d3a2d7
The-BoxHead-Guy <astralgmd@gmail.com>
2025-02-27 17:57:12 -0400

    feat: :construction: stylize the new cart item when it exists in cart

--- src/components/reactComponents/Cart.jsx
+++ src/components/reactComponents/Cart.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 
 const Cart = () => {
+  let testElement = {
+    thumbnail: "/src/assets/images/image-product-1-thumbnail.jpg",
+    name: "Fall Limited Edition Sneakers",
+    price: 125,
+    quantity: 3,
+  };
+
   return (
     <div
-      className="hidden fixed z-10 w-full min-h-1/6 h-64 max-h-1/3 mx-auto font-kumbh"
+      className="fixed z-10 w-full min-h-1/6 h-auto mx-auto font-kumbh"
       id="cart"
     >
       <div className="m-4 bg-white rounded-lg h-full">
@@ -12,10 +19,24 @@ const Cart = () => {
             <h3 className="text-lg font-semibold">Cart</h3>
           </header>
           <hr className="border-b-2 opacity-5" />
-          <section className="flex items-center justify-center h-full">
-            <article className="text-gray-500 font-semibold">
-              Your Cart is empty.
-            </article>
+          <section className="flex items-center justify-center flex-col h-full">
+            {testElement ? (
+              <div className="w-full px-4 gap-4 flex flex-col justify-between">
+                <article className="flex items-center gap-2">
+                  <div className="grow-1">
+                    <img src={testElement.thumbnail} alt="" />
+                  </div>
+                  <div className="grow-1">items</div>
+                </article>
+                <button className="w-full bg-primary p-4 rounded-lg text-gray-900 font-semibold text-lg">
+                  Checkout
+                </button>
+              </div>
+            ) : (
+              <article className="text-gray-500 font-semibold">
+                Your Cart is empty.
+              </article>
+            )}
           </section>
         </div>
       </div>
