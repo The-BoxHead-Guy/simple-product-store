1d11598672548cbb9df8b66dee9cef67a000eaeb
The-BoxHead-Guy <astralgmd@gmail.com>
2025-03-04 12:20:18 -0400

    refactor: update product constant with imported Product type and additional properties

--- src/constants/products/properties.ts
+++ src/constants/products/properties.ts
@@ -1,13 +1,7 @@
-type Product = {
-  company: string;
-  title: string;
-  description: string;
-  price: number;
-  originalPrice: number;
-  discount: number;
-};
+import type { Product } from "../../types/Product";
 
 const product: Product = {
+  id: 1,
   company: "Sneaker Company",
   title: "Fall Limited Edition Sneakers",
   description:
@@ -15,6 +9,7 @@ const product: Product = {
   price: 125.0,
   originalPrice: 250.0,
   discount: 50,
+  image: "/images/image-product-1.jpg",
 };
 
 export default product;
