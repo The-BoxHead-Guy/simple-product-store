ab5ebf9d8c3a2dd73213acf2b1153330d022dea1
The-BoxHead-Guy <astralgmd@gmail.com>
2025-03-04 12:19:13 -0400

    refactor: update CartButton to use Zustand cart state

--- src/components/reactComponents/buttons/CartButton.tsx
+++ src/components/reactComponents/buttons/CartButton.tsx
@@ -1,25 +1,25 @@
 import { useEffect } from "react";
 import cartIcon from "../../../assets/icons/icon-cart.svg";
+import { useCartStore } from "../../../stores/cartStore";
 
 type Cart = HTMLElement | null;
 
 function CartButton() {
+  const { toggleCart, totalItems } = useCartStore();
   let cartElement: Cart;
 
   useEffect(() => {
     cartElement = document.getElementById("cart");
   });
 
-  const findElement = () => {
-    cartElement?.classList.toggle("hidden");
-  };
-
   return (
-    <button className="relative" aria-label="Cart" onClick={findElement}>
+    <button className="relative" aria-label="Cart" onClick={toggleCart}>
       <img src={cartIcon.src} alt="" />
-      <span className="absolute -top-2 -right-2 bg-orange-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
-        3
-      </span>
+      {totalItems > 0 && (
+        <span className="absolute -top-2 -right-2 bg-orange-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
+          {totalItems}
+        </span>
+      )}
     </button>
   );
 }
